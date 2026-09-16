7ac0252ea3d0a134bda2452fb639a95a5dd851e6
pau-ovando <ovandos.paulina.p8@gmail.com>
2026-09-15 23:41:58 -0600

    CRUD con MongoDB

--- repositorios/pizza.repositorio.js
+++ repositorios/pizza.repositorio.js
@@ -1,70 +1,28 @@
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+import { MongoClient, ObjectId } from 'mongodb';
 
-let pizzas = [
-    { id : 1, nombre: "Hawaiiana", descripcion: "Pizza con piña y jamón" }
-]
+const cliente = new MongoClient("mongodb://root:123@localhost:27017/");
+const db = cliente.db("prueba"); 
+const pizzasDb = db.collection("pizzas"); 
 
-/**
- * Regresa todas las pizzas
- * @returns {Array} pizzas
- */
 export async function obtenerTodasLasPizzasAsync() {
-    await sleep(2000); // Simula un retraso de 2 segundos
-    
-    return pizzas;
+    return await pizzasDb.find({}).toArray();
 }
 
-/**
- * Regresa la pizza del id buscado
- * @param {*} id
- * @returns pizza
- */
 export async function obtenerPizzaPorIdAsync(id) {
-    await sleep(1000);
-    const pizza = pizzas.find(x => x.id == id);
-
-    return pizza;
+    return await pizzasDb.findOne({ _id: new ObjectId(id) });
 }
 
-/**
- * Agrega una pizza al arreglo de pizzas
- * @param {*} pizza
- * @returns id de la pizza agregada
- */
 export async function agregarPizzaAsync(pizza) {
-    await sleep(1000);
-    pizzas.push(pizza);
-
-    return pizzas.length // Devuelve el id de la pizza agregada
+    const resultado = await pizzasDb.insertOne(pizza);
+    return resultado.insertedId;
 }
 
-/**
- * Actualiza la pizza del id buscado
- * @param {*} id
- * @param {*} pizza
- * @returns pizza actualizada o undefined si no se encuentra
- */
 export async function actualizarPizzaAsync(id, pizza) {
-    const index = pizzas.findIndex(x => x.id == id);
-    if(index == -1)
-        return undefined
-    pizzas[index].nombre = pizza.nombre;
-    pizzas[index].descripcion = pizza.descripcion;
-
-    return pizzas[index];
+    delete pizza._id; // Evita el error de intentar modificar el _id inmutable
+    return await pizzasDb.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: pizza }, { returnDocument: 'after' });
 }
 
-/**
- * Elimina la pizza del id buscado
- * @param {*} id
- * @returns true si se eliminó, false si no se encontró
- */
 export async function eliminarPizzaAsync(id) {
-    // await sleep(1000);
-    const index = pizzas.findIndex(x => x.id == id);
-    if (index === -1) {
-        return false;
-    }
-    pizzas.splice(index, 1); // Se agrega el "1" para decirle a splice que solo elimine 1 elemento
-    return true;
+    const resultado = await pizzasDb.deleteOne({ _id: new ObjectId(id) });
+    return resultado.deletedCount > 0;
 }
